4a62f5e365550940a867ad11d2fee29e2e117382
Vraj <vrajnshah05@gmail.com>
2023-05-27 12:51:55 +0530

    Started working on the Project

---
+++ README1.md
@@ -0,0 +1,17 @@
+## COVID-19 TRACKER APPLICATION BUILD
+
+Here, we will use the BEM naming convention
+
+# Drawio
+This is where you draw the main wireframe for any huge projects
+
+--> Now we create the project with
+    npx reate-react-app covid-19-tracker
+
+# Material UI
+We will make this project using Material UI, the world's most popular React UI framework.
+MUI offers a comprehensive suite of UI tools to help you ship new features faster.
+Type this in the terminal to download all the dependencies from there into this project
+    npm install @mui/material @emotion/react @emotion/styled
+
+Continue from 02:00:31
--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { FormControl, Select, MenuItem, Card, CardContent }   from '@mui/material';
 import './App.css';
+import InfoBox from './InfoBox';
+import Map from './Map';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch ("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => (
+            {
+              name: country.country,
+              value: country.countryInfo.iso2
+            }
+          ));
+
+          setCountries(countries);
+        })
+    };
+
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+
+    setCountry(countryCode);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='app__left'>
+        <div className='app__header'>
+          <h1>COVID-19 TRACKER</h1>
+          {/* This is present in the Material UI */}
+          <FormControl className="app__dropdown">
+            <Select variant="outlined" value={country} onChange={onCountryChange}>
+              <MenuItem value="worldwide">WorldWide</MenuItem>
+              {/* Looping thorugh all the countries and showing a dropdown menu for the options */}
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div class="app__stats">
+          <InfoBox title="CoronaVirus Cases" />
+          <InfoBox title="Recovered" />
+          <InfoBox title="Deaths" />
+        </div>
+
+        <Map />
+      </div>
+      <Card className='app__right'></Card>
     </div>
   );
 }
